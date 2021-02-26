0e2ebd13c05b82b130eaeae8523b8177814ed533
denisSimonov <deniic@mail.ru>
2021-02-26 10:41:58 +0300

    App init

---
+++ src/Dictionary/Dictionary/Controller/WordsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dictionary.Controller
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordsController : ControllerBase
+    {
+        [HttpGet("get")]
+        public async Task<ActionResult> GetReport()
+        {
+            return await Task.Run(Ok) ;
+        }
+    }
+}
